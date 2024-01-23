6c1be74056a31b8d72ef7ced910934e5df6c0c1f
mask <lekiet20050@gmail.com>
2024-01-23 21:12:45 +0700

    init project

--- cong_hai_so/cong_overflow.asm
+++ cong_hai_so/cong_overflow.asm
@@ -1,13 +1,13 @@
 include C:\masm32\include\masm32rt.inc
 
 .data
-    msg1 db "Nhap so thu nhat(dec): ", 0
-    msg2 db "Nhap so thu hai(dec): ", 0
-    msg3 db "Ket qua(hex): ", 0
-    x db 20 dup(?)
-    y db 20 dup(?)
-    sum_result dd 100 dup(0)
-    sum dd 100 dup(0)
+    msg1 db "Nhap so thu nhat: ", 0
+    msg2 db "Nhap so thu hai: ", 0
+    msg3 db "Ket qua: ", 0
+    x db 22 dup(?)
+    y db 22 dup(?)
+    sum_result db 100 dup(0)
+    sum db 100 dup(0)
     tmp dd ?
 
 .code
@@ -55,36 +55,23 @@ chuyen_sang_string proc
     push esi
     push edx
     lea esi, sum     ;Trỏ esi đến địa chỉ của sum
-    mov ebx, 16      ;Cho ebx=10 để thực hiện phép chia
-    
+    mov ebx, 10      ;Cho ebx=10 để thực hiện phép chia
+
     lap_string:
         cmp eax, 0        ;Kiểm tra nếu giá trị trong thanh ghi eax=0 thì kết thúc vòng lặp
         je out_string     
 
         mov edx, 0        ;Cho edx=0 để reset lại giá trị của edx sau khi chia
         div ebx           ;chia eax cho 10
-        call chuyen_hex
+        add dl, 48        ;chuyển giá trị trong dl(số dư sau khi chia) sang số dạng kí tự
+        mov byte ptr [esi], dl   ;Chuyển kí tự trong dl vào địa chỉ của biến sum
         inc esi           ;Tăng esi 1 đơn vị để trỏ đến địa chỉ tiếp theo
         jmp lap_string
 
     out_string:
-
-    pop edx  ;chuyển sang kí tự với phần bit lớn lưu trong edx sau khi nhân
-    mov eax,edx
-    lap_string_2:
-        cmp eax, 0        ;Kiểm tra nếu giá trị trong thanh ghi eax=0 thì kết thúc vòng lặp
-        je out_string_2     
-
-        mov edx, 0        ;Cho edx=0 để reset lại giá trị của edx sau khi chia
-        div ebx           ;chia eax cho 10
-        call chuyen_hex
-        inc esi           ;Tăng esi 1 đơn vị để trỏ đến địa chỉ tiếp theo
-        jmp lap_string_2
-
-    out_string_2:
     call dao_nguoc      ;gọi hàm dao_nguoc để đưa về đúng kết quả
-            
-    pop esi    ;Trả lại các giá trị trong thanh ghi lúc đầu
+    pop edx       ;Trả lại các giá trị trong thanh ghi lúc đầu
+    pop esi
     pop ecx
     pop ebx
     ret
@@ -132,94 +119,12 @@ dao_nguoc proc
     ret
 dao_nguoc endp
 ;------------------
-;------------------
-chuyen_hex proc
-    cmp dl,0              ;so sánh lần lượt số dư với các số từ 0->15 nếu bằng cái nào thì chuyển sang số hệ 16 tương ứng
-    jne next_1
-    mov byte ptr[esi],48
-    jmp continue
-next_1:
-    cmp dl,1
-    jne next_2
-    mov byte ptr[esi],49
-    jmp continue
-next_2:
-    cmp dl,2
-    jne next_3
-    mov byte ptr[esi],50
-    jmp continue
-next_3:
-    cmp dl,3
-    jne next_4
-    mov byte ptr[esi],51
-    jmp continue
-next_4:
-    cmp dl,4
-    jne next_5
-    mov byte ptr[esi],52
-    jmp continue
-next_5:
-    cmp dl,5
-    jne next_6
-    mov byte ptr[esi],53
-    jmp continue
-next_6:
-    cmp dl,6
-    jne next_7
-    mov byte ptr[esi],54
-    jmp continue
-next_7:
-    cmp dl,7
-    jne next_8
-    mov byte ptr[esi],55
-    jmp continue
-next_8:
-    cmp dl,8
-    jne next_9
-    mov byte ptr[esi],56
-    jmp continue
-next_9:
-    cmp dl,9
-    jne next_10
-    mov byte ptr[esi],57
-    jmp continue
-next_10:
-    cmp dl,10
-    jne next_11
-    mov byte ptr[esi],97
-    jmp continue
-next_11:
-    cmp dl,11
-    jne next_12
-    mov byte ptr[esi],98
-    jmp continue
-next_12:
-    cmp dl,12
-    jne next_13
-    mov byte ptr[esi],99
-    jmp continue
-next_13:
-    cmp dl,13
-    jne next_14
-    mov byte ptr[esi],100
-    jmp continue
-next_14:
-    cmp dl,14
-    jne next_15
-    mov byte ptr[esi],101
-    jne next_15
-    jmp continue
-next_15:
-    mov byte ptr[esi],102
-    jmp continue
-    continue:
-    ret
-chuyen_hex endp
-;---------------
+
+
 start:
     ; Nhập số thứ nhất
     invoke StdOut,addr msg1
-    push 11
+    push 22
     push offset x
     call StdIn
 
@@ -227,23 +132,25 @@ start:
     call chuyen_sang_int 
     
     mov tmp, eax    ;cho tmp=eax để lưu trữ giá trị của x cho việc tính toán tiếp theo
-    
+
    ; Nhập số thứ hai
     invoke StdOut, addr msg2
-    push 11
+    push 22
     push offset y
     call StdIn
     mov eax, offset y
     call chuyen_sang_int
     
-    mov ebx,tmp   ;Cho ebx=tmp để thực hiện phép nhân
-    mul ebx       ;Nhân giá trị trong eax với ebx (=x*y)
+    mov edx,tmp
+    add eax,edx
     call chuyen_sang_string
 
     ; Hiển thị kết quả
     invoke StdOut, addr msg3
     invoke StdOut, offset sum_result
-    ; Thoát chương trình
+
+    ; Exit the program
     invoke ExitProcess, 0
 
 end start
+
